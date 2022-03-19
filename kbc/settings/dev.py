782f2deb474ddaa107308bcabf874e98b0b9be6a
LadPaule <ssozipaule@gmail.com>
2022-03-19 15:40:17 +0300

    commit1

---
+++ kbc/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-t6svsvq!q+85mdau9r)l4h6+fdjt0g^^$=1_fuv1g+-a9*&3@$'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
