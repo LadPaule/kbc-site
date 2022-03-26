9cc486561da3644da554e21ed26a1e1dfb2104d9
LadPaule <ssozipaule@gmail.com>
2022-03-26 22:26:39 +0300

    index page

--- home/models.py
+++ home/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
+from modelcluster.fields import ParentalKey
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
 
 
 
 class HomePage(Page):
     body = RichTextField(blank=True)
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
+        FieldPanel('body', classname="full", help_text="This is the body of the page"),
+        InlinePanel('gallery_images', label="Gallery images", help_text="Upload images to the carousel"),
+    ]
+class HomePageGalleryImage(Orderable):
+    page = ParentalKey('HomePage', on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey('wagtailimages.Image', on_delete=models.CASCADE, related_name='+')
+    caption = models.CharField (blank=True, max_length=800)
+    image_title= models.CharField(blank=True, max_length=250)
+
+    panels = [ ImageChooserPanel('image'),
+        FieldPanel('caption'),
+        FieldPanel('image_title'),
     ]
