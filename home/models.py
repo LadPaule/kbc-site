07f3323cd1be71c1d8667f59e6ac5542c5b3f365
LadPaule <ssozipaule@gmail.com>
2022-03-28 20:49:17 +0300

    minor changes

--- home/models.py
+++ home/models.py
@@ -3,7 +3,8 @@ from django.db import models
 from wagtail.core.models import Page, Orderable
 from modelcluster.fields import ParentalKey
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, FieldRowPanel
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 
@@ -18,6 +19,7 @@ class HomePage(Page):
         InlinePanel('gallery_images', label="Carousel or Slider images", help_text="Upload images to the carousel"),
     ]
 class HomePageGalleryImage(Orderable):
+    DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
     page = ParentalKey('HomePage', on_delete=models.CASCADE, related_name='gallery_images')
     image = models.ForeignKey('wagtailimages.Image', on_delete=models.CASCADE, related_name='+')
     caption = models.CharField (blank=True, max_length=800)
@@ -37,6 +39,7 @@ class InceptionPage(Page):
     ]
 
 class InceptionPageGalleryImage(Orderable):
+    DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
     page = ParentalKey('InceptionPage', on_delete=models.CASCADE, related_name='Heroimages')
     image = models.ForeignKey('wagtailimages.Image', on_delete=models.CASCADE, related_name='+')
     side_image = models.ForeignKey('wagtailimages.Image', blank=True, null=True, on_delete=models.CASCADE, related_name='+')
@@ -47,3 +50,93 @@ class InceptionPageGalleryImage(Orderable):
         FieldPanel('caption'),
         FieldPanel('image_title'),
     ]
+
+
+class ContactPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel('intro', classname="full", help_text="This is the intro of the Contact Page"),
+        InlinePanel('contact_page_form_fields', label="Contact Form Fields", help_text="Add Contact Form Fields"),
+        FieldPanel('thank_you_text', classname="full", help_text="This is the appreciative text of the Contact Page"),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel('subject'),
+        ], heading="Email Settings"),
+    ]
+
+class FormField(AbstractFormField):
+    DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+    page = ParentalKey('ContactPage', on_delete=models.CASCADE, related_name='contact_page_form_fields')
+
+
+# Prayer Request Page
+class PrayerRequestPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel('intro', classname="full", help_text="This is the intro of the Contact Page"),
+        InlinePanel('Heroimage', label="Carousel or Slider images", help_text="Upload images to the carousel"),
+        InlinePanel('prayer_request_page_form_fields', label="Contact Form Fields", help_text="Add Contact Form Fields"),
+        FieldPanel('thank_you_text', classname="full", help_text="This is the appreciative text of the Contact Page"),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel('subject'),
+        ], heading="Email Settings"),
+    ]
+
+class PrayerRequestPageFormField(AbstractFormField):
+    DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+    page = ParentalKey('PrayerRequestPage', on_delete=models.CASCADE, related_name='prayer_request_page_form_fields')
+
+class PrayerRequestPageGalleryImage(Orderable):
+    DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+    page = ParentalKey('PrayerRequestPage', on_delete=models.CASCADE, related_name='Heroimage')
+    image = models.ForeignKey('wagtailimages.Image', on_delete=models.CASCADE, related_name='+')
+    caption = models.CharField (blank=True, max_length=800)
+    image_title= models.CharField(blank=True, max_length=250)
+
+    panels = [ ImageChooserPanel('image'),
+        FieldPanel('caption'),
+        FieldPanel('image_title'),
+    ]
+
+
+# Appointments Page
+class AppointmentsPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel('intro', classname="full", help_text="This is the intro of the Contact Page"),
+        InlinePanel('Heroimage', label="Carousel or Slider images", help_text="Upload images to the carousel"),
+        InlinePanel('prayer_request_page_form_fields', label="Appointment Form Fields", help_text="Add Contact Form Fields"),
+        FieldPanel('thank_you_text', classname="full", help_text="This is the appreciative text of the Contact Page"),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel('subject'),
+        ], heading="Email Settings"),
+    ]
+class AppointmentsPageFormField(AbstractFormField):
+    DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+    page = ParentalKey('AppointmentsPage', on_delete=models.CASCADE, related_name='prayer_request_page_form_fields')
+
+class AppointmentsPageGalleryImage(Orderable):
+    page = ParentalKey('AppointmentsPage', on_delete=models.CASCADE, related_name='Heroimage')
+    image = models.ForeignKey('wagtailimages.Image', on_delete=models.CASCADE, related_name='+')
+    caption = models.CharField (blank=True, max_length=800)
+    image_title= models.CharField(blank=True, max_length=250)
+
+    panels = [ ImageChooserPanel('image'),
+        FieldPanel('caption'),
+        FieldPanel('image_title'),
+    ]
+
---
+++ home/wagtail_hooks.py
@@ -0,0 +1,68 @@
+from wagtail.core import hooks
+import wagtail.admin.rich_text.editors.draftail.features as draftail_features
+from wagtail.admin.rich_text.converters.html_to_contentstate import InlineStyleElementHandler
+
+@hooks.register('register_rich_text_features')
+def register_code_styling(features):
+# Add the code feature to the list of features
+  feature_name="code"
+  type_="CODE"
+  tag="code"
+
+  control ={
+    "type": type_,
+    "label": "</>",
+    "description": "Code",
+  }
+  features.register_editor_plugin(
+    "draftail", feature_name, draftail_features.InlineStyleFeature(control)
+  )
+
+  db_conversion ={
+    "from_database_format": {tag: InlineStyleElementHandler(type_)},
+    "to_database_format": {"Style_map": {type_: {"element": tag}}}
+  }
+
+  features.register_converter_rule("contentstate", feature_name, db_conversion)
+
+  # This is the default rule for all features
+  features.default_features.append(feature_name)
+
+
+@hooks.register('register_rich_text_features')
+def register_centertext_feature(features):
+  # create centered text in the editor panel
+  feature_name="centertext"
+  type_="CENTER"
+  tag="div"
+
+  control ={
+    "type": type_,
+    "label": "Center",
+    "description": "Center Text",
+    "style": {
+      "dispay": "block",
+      "text-align": "center",
+    }
+  }
+  features.register_editor_plugin(
+    "draftail", feature_name, draftail_features.InlineStyleFeature(control)
+  )
+
+  db_conversion ={
+    "from_database_format": {tag: InlineStyleElementHandler(type_)},
+    "to_database_format": {"style_map": {
+      type_: {
+        "element": tag,
+        "props": {
+          "class": "d-block text-center"
+        }
+        }
+      }
+      }
+  }
+
+  features.register_converter_rule("contentstate", feature_name, db_conversion)
+
+  # This is the default rule for all features
+  features.default_features.append(feature_name)
