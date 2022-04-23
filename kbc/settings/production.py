1321647ea4e70216a82c68a00e2d291654e991db
LadPaule <ssozipaule@gmail.com>
2022-04-23 22:28:53 +0300

    prep for production

--- kbc/settings/production.py
+++ kbc/settings/production.py
@@ -1,6 +1,13 @@
+from __future__ import absolute_import, unicode_literals
 from .base import *
+import dj_database_url
 
 DEBUG = False
+DATABASES['default'] = dj_database_url.config()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
 RECAPTCHA_PUBLIC_KEY = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
 RECAPTCHA_PRIVATE_KEY = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
 NOCAPTCHA = True
