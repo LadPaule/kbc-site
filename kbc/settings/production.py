0db65bd54666e55e40ef0ba42c5a2dc5519856f8
LadPaule <ssozipaule@gmail.com>
2022-04-24 08:08:35 +0300

    production test

--- kbc/settings/production.py
+++ kbc/settings/production.py
@@ -6,11 +6,16 @@ DEBUG = False
 DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+ROOT_URLCONF = 'kbc.urls.py'
+
+SECRET_KEY = 'h7749i^i*3h5fnm@g-el82j4sj!45si!h-@1ejh@9c16y79&6-'
+
 ALLOWED_HOSTS = ['*']
 
 RECAPTCHA_PUBLIC_KEY = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
 RECAPTCHA_PRIVATE_KEY = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
 NOCAPTCHA = True
+
 
 try:
     from .local import *
